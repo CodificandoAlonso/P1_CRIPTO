5ba9e9d99fb5272ee45c56cf12ee0688d0e8b303
Hector <100495794@alumnos.uc3m.es>
2024-09-19 18:42:57 +0200

    Coño commiteate

--- main.py
+++ main.py
@@ -4,12 +4,39 @@ from user import user
 
 class App():
     def __init__(self):
-        self.users = []
+        print('Welcome to the app, if anytime you want to exit, press Ctrl + C')
+        self.run()
 
-    def login(self):
+    def signup(self):
         username = input('Enter username: ')
         password = input('Enter password: ')
         user(username, password)
+
+
+    def login(self):
+        print("Logged in")
+
+
+    #que la app esté siempre activa salvo que se pulse ctrl+c
+    def run(self):
+        while True:
+            try:
+                myinput = input("Hi. If you already have a registered username please type 'Log in' \n If you otherwise want to sign up, please type 'Sign up': ")
+                print( myinput)
+                while  myinput != "Log in" and myinput != "Sign up":
+                    myinput = input("Please type 'Log in' or 'Sign up': ")
+                if myinput == "Sign up":
+                    self.signup()
+                else:
+                    self.login()
+                
+            except KeyboardInterrupt:
+                print('Goodbye')
+                break
+
+
+
+
 
 
 app = App()
--- user.py
+++ user.py
@@ -6,7 +6,7 @@ class user():
     def __init__(self, username, password):
         self.username = validate_user(username)
         self.password = validate_password(password)
-        #save_user()
+        save_user(self.username, self.password)
 
 
 def validate_user(username):
@@ -14,20 +14,40 @@ def validate_user(username):
         print('Username must be at least 5 characters long')
         username = input('Enter username again: ')
     
-    """with open('jsones/users.json', 'r') as file:
-        users = json.load(file)
-        while username in users:
-            print('Username already exists')
-            username = input('Enter username again: ')"""
+    with open('jsones/users.json', 'r') as file:
+        if not file.read().strip() == "":
+                with open("jsones/users.json", "r", encoding="utf-8") as file:
+                    users = json.load(file)
+                while any(d['username'] == username for d in users):
+                    print('Username already exists')
+                    username = input('Enter username again: ')
+        return username
 
-def validate_password(password):
-    while len(password) < 8 or not any(char.isdigit() for char in password) or not any(char.isupper() for char in password):
+def has_special_char(password):
+    elem = r'[^\w\s]'
+    return bool(re.search(elem, password))
+
+def validate_password( password):
+    while len(password) < 8 or not any(char.isdigit() for char in password) or not any(char.isupper() for char in password) or not has_special_char(password):
         print('Password must be at least 8 characters long and contain at least one uppercase letter, one digit and one special character')
         password = input('Enter password again: ')
     print("Your password has been saved carapito")
-    #return password
+    return password
 
 
-#or not any (bool(re.search(r'[^a-zA-Z0-9]', password))):
 
-#def save_user():
+def save_user(username, password):
+    try:
+        with open('jsones/users.json', 'r', encoding='utf-8') as file:
+            try:
+                users = json.load(file)
+            except json.JSONDecodeError:
+                users = []
+    except FileNotFoundError:
+        users = []
+
+    users.append({'username': username, 'password': password})
+
+    with open('jsones/users.json', 'w', encoding='utf-8') as file:
+        json.dump(users, file, indent=4)
+    print('User has been saved')
