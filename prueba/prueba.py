0c3e59e304385dc0ddc4b811dc25df58b9e43a35
Baisicol <100495764@alumnos.uc3m.es>
2024-10-24 13:01:54 +0200

    Soy un genio, private key está como atributo y la public me la he guardado, ahora solo me queda encriptar y desencriptar las claves simetricas

--- prueba/prueba.py
+++ prueba/prueba.py
@@ -9,7 +9,7 @@ from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 # Generar un par de claves RSA
 private_key = rsa.generate_private_key(
     public_exponent=65537,
-    key_size=2048,
+    key_size=4096,
 )
 
 public_key = private_key.public_key()
